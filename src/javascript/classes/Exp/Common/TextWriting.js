1f7e9f3a0e2b20eb9f4c1d35ef48944d4731b6a7
BAUDRY Alicia <abaudry@p-champenois-p.intra.gobelins.fr>
2018-04-04 16:32:06 +0200

    test text writing

---
+++ src/javascript/classes/Exp/Common/TextWriting.js
@@ -0,0 +1,30 @@
+class TextWriting {
+    constructor(options) {
+		Storage.TextWriting = this
+   
+		this.init()
+    }
+
+	init = () => {
+		let options1 = {
+		  strings: ["La Corée du Nord c'est trop fort", "Kom le rokfor."],
+		  typeSpeed: 50,
+		  loop: true,
+		}
+
+		let options2 = {
+		  strings: ["La Corée du Nord c'est trop fort"],
+		  typeSpeed: 50,
+		  loop: false,
+		  loopCount: 1,
+		}
+
+		let typed1 = new Typed(".test_typedjs_1", options1)
+		let typed2 = new Typed(".test_typedjs_2", options2)
+
+	}
+
+
+}
+
+export default TextWriting
