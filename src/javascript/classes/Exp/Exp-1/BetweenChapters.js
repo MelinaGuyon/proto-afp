83d56c27b13f990d8e088ed491aef50e362498a6
Ptikiki <alicia.baudry.31@gmail.com>
2018-05-10 18:50:21 +0200

    fix voice over and additional text for chapt1

--- src/javascript/classes/Exp/Exp-1/BetweenChapters.js
+++ src/javascript/classes/Exp/Exp-1/BetweenChapters.js
@@ -10,11 +10,11 @@ class BetweenChapters {
 
     initFcTable = () => {
       this.table = [
-        [ this.launchVoiceOver(0), this.launchVoiceOver(1), this.launchText(0), this.launchText(1) ],
+        [ this.launchVoiceOver(0), this.launchVoiceOver(1), this.launchText(0) ],
 
-        [ this.launchVoiceOver(2), this.launchVoiceOver(3), this.launchConclusion(1), this.stopConclusion ],
+        [ this.launchVoiceOver(2), this.launchVoiceOver(3), this.launchText(1), this.launchConclusion(1), this.stopConclusion ],
 
-        [ this.launchText(2), this.launchVoiceOver(4), this.launchVoiceOver(5), this.launchConclusion(2), this.stopConclusion ],
+        [ this.launchVoiceOver(4), this.launchVoiceOver(5),  this.launchText(2), this.launchConclusion(2), this.stopConclusion ],
 
         [ this.launchVoiceOver(6), this.launchVoiceOver(7), this.launchConclusion(3), this.stopConclusion ],
 
