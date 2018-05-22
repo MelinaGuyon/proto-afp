205b9626cb13cae2a6f1d424d845064b7f83d64b
GUYON Mélina <mguyon@g-a101-28.local>
2018-05-22 11:16:02 +0200

    Complete sound manager

--- src/javascript/classes/Exp/Common/SoundManager.js
+++ src/javascript/classes/Exp/Common/SoundManager.js
@@ -8,15 +8,16 @@ class SoundManager {
 			this.state = options
 
       this.voiceOver
+      this.ambianceSound
+      this.backgroundMusic
     }
 
     launchVoiceOver = (track) => {
-      if (this.voiceOver) this.updateSrc(track)
-      else this.creatNew(track)
+      if (this.voiceOver) this.updateVoiceOverSrc(track)
+      else this.createNewVoiceOver(track)
     }
 
-    creatNew = (track) => {
-      console.log(track)
+    createNewVoiceOver = (track) => {
       this.voiceOver = new Audio(track)
       this.voiceOver.volume = 0
       this.voiceOver.play()
@@ -28,8 +29,7 @@ class SoundManager {
       })
     }
 
-    updateSrc = (track) => {
-      console.log(track)
+    updateVoiceOverSrc = (track) => {
       this.voiceOver.pause()
       this.voiceOver.src = track
       this.voiceOver.volume = 0
@@ -39,6 +39,80 @@ class SoundManager {
         volume: [0, 1],
         duration: 600,
         easing: 'linear'
+      })
+    }
+
+    launchAmbianceSound = (track) => {
+      if (this.ambianceSound) this.updateAmbianceSrc(track)
+      else this.createNewAmbiance(track)
+    }
+
+    createNewAmbiance = (track) => {
+      this.ambianceSound = new Audio(track)
+      this.ambianceSound.volume = 0
+      this.ambianceSound.play()
+      anime({
+        targets: this.ambianceSound,
+        volume: [0, 1],
+        duration: 600,
+        easing: 'linear'
+      })
+    }
+
+    updateAmbianceSrc = (track) => {
+      anime({
+        targets: this.ambianceSound,
+        volume: [1, 0],
+        duration: 600,
+        easing: 'linear',
+        complete: () => {
+          this.ambianceSound.pause()
+          this.ambianceSound.src = track
+          this.ambianceSound.play()
+          anime({
+            targets: this.ambianceSound,
+            volume: [0, 1],
+            duration: 600,
+            easing: 'linear'
+          })
+        }
+      })
+    }
+
+    launchBackgroundMusic = (track) => {
+      if (this.backgroundMusic) this.updateBackgroundMusicSrc(track)
+      else this.createNewBackgroundMusic(track)
+    }
+
+    createNewBackgroundMusic = (track) => {
+      this.backgroundMusic = new Audio(track)
+      this.backgroundMusic.volume = 0
+      this.backgroundMusic.play()
+      anime({
+        targets: this.backgroundMusic,
+        volume: [0, 1],
+        duration: 600,
+        easing: 'linear'
+      })
+    }
+
+    updateBackgroundMusicSrc = (track) => {
+      anime({
+        targets: this.backgroundMusic,
+        volume: [1, 0],
+        duration: 600,
+        easing: 'linear',
+        complete: () => {
+          this.backgroundMusic.pause()
+          this.backgroundMusic.src = track
+          this.backgroundMusic.play()
+          anime({
+            targets: this.backgroundMusic,
+            volume: [0, 1],
+            duration: 600,
+            easing: 'linear'
+          })
+        }
       })
     }
 }
